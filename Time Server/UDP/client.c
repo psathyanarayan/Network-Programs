fe80fe288aa16d197fba55c5c5693f174ecb21e8
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2022-09-21 16:26:22 +0530

    Add files via upload

---
+++ Time Server/UDP/client.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<netinet/in.h>
+int main()
+{
+	int clientsock;
+	struct sockaddr_in serveraddr;
+	char message[50];
+	socklen_t len;
+	
+	clientsock = socket(AF_INET,SOCK_DGRAM,0);
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(8000);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	len = sizeof(serveraddr);
+	sendto(clientsock,"THIS IS ROOT",sizeof("THIS IS ROOT"),0,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+	recvfrom(clientsock,message,29,0,(struct sockaddr*)&serveraddr,&len);
+	printf("%s",message);
+	close(clientsock);
+}
---
+++ Time Server/UDP/server.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<sys/types.h>
+#include<time.h>
+int main(int argc, char *argv[]) {
+	int serversock;
+	struct sockaddr_in serveraddr,clientaddr;
+	socklen_t len;
+	char message[50];
+	time_t currentTime;
+	time(&currentTime);
+	serversock = socket(AF_INET,SOCK_DGRAM,0);
+	bzero((char *)&serveraddr,sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(8000);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	
+	bind(serversock,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+	bzero((char *)&clientaddr,sizeof(clientaddr));
+	len = sizeof(clientaddr);
+	
+	recvfrom(serversock,message,sizeof(message),0,(struct sockaddr*)&clientaddr,&len);
+	printf("%s",message);
+	sendto(serversock,ctime(&currentTime),30,0,(struct sockaddr*)&clientaddr,sizeof(clientaddr));
+	close(serversock);
+	
+}
